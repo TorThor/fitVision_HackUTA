6461e29ecee65390c91724ad0777ded19e32ec83
Tor Qureshi <torqureshi@gmail.com>
2024-10-12 19:11:25 -0500

    Add open camera

--- backend/vision/keypoint_detection.py
+++ backend/vision/keypoint_detection.py
@@ -1,9 +1,22 @@
-import kagglehub
 import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
 
-# Download latest version
-movenet_dir = os.path.join(os.getcwd(), 'backend\MoveNet_model')
-# print("MoveNet dir:", movenet_dir)
-os.chdir(movenet_dir)
-path = kagglehub.model_download("google/movenet/tfLite/singlepose-thunder")
+movenet_path = os.path.join(os.getcwd(), 'backend', 'MoveNet_model', '3.tflite')
+# print("Movenet path:", movenet_path)
+interpreter = tf.lite.Interpreter(model_path=movenet_path)
+interpreter.allocate_tensors()
 
+cap = cv2.VideoCapture(1)
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    cv2.imshow('MoveNet Thunder', frame)
+
+    if cv2.waitKey(10) & 0xFF==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
